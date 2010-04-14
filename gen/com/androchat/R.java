8b4b31681a9903dd27f1cece8a2817bfc1719194
shaulian <shaulian@9d925df9-d79c-6c44-8a10-6f126a3ac406>
2010-04-14 15:38:18 +0000

    Change Manifest to work and r.java

--- gen/com/androchat/R.java
+++ gen/com/androchat/R.java
@@ -13,10 +13,29 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int AddContact=0x7f05000b;
+        public static final int AddresseeName=0x7f05000a;
+        public static final int EditText01=0x7f050002;
+        public static final int EditText02=0x7f050000;
+        public static final int EditText03=0x7f050004;
+        public static final int MessageBody=0x7f05000e;
+        public static final int Spinner01=0x7f050005;
+        public static final int TableLayout01=0x7f050006;
+        public static final int TableRow01=0x7f050009;
+        public static final int TableRow02=0x7f050007;
+        public static final int TableRow03=0x7f05000c;
+        public static final int TableRow04=0x7f05000d;
+        public static final int TextView01=0x7f050008;
+        public static final int View01=0x7f050003;
+        public static final int ViewStub01=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int newmessage=0x7f030001;
     }
     public static final class string {
+        public static final int app_desc=0x7f040002;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
     }
