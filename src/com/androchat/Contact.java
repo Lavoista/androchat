48e84504e06fb7fac1e00ed030967e5e3c6e603f
dov.itskovich <dov.itskovich@9d925df9-d79c-6c44-8a10-6f126a3ac406>
2010-05-17 21:11:35 +0000

    Update Core

--- src/com/androchat/Contact.java
+++
@@ -1,5 +0,0 @@
-package com.androchat;
-
-public class Contact {
-
-}
---
+++ src/com/androchat/TwitterManagerTest.java
@@ -0,0 +1,41 @@
+package com.androchat;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import twitter4j.*;
+
+public class TwitterManagerTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length < 2) {
+            System.out.println("No TwitterID/Password specified.");
+            System.out.println(
+                "Usage: java com.androchat.TwitterManagerTest ID Password");
+            System.exit( -1);
+        }
+		System.out.println("Test");
+		try {
+			TwitterManager twitter = TwitterManager.getInstance();		
+			twitter.Connect(args[0], args[1], 10);
+			ArrayList<User> arr = twitter.GetAllContacts();
+			for(User u : arr){
+				System.out.println("msg for " + u.getScreenName() + ":");
+				twitter.SendMessage(u.getScreenName(), "Test");
+				for(DirectMessage msg : twitter.GetMessagesForContact(u.getScreenName())){
+					System.out.println(msg.getSender().getScreenName() + " " + msg.getCreatedAt().toString() + " : " + msg.getText());
+				}
+			}
+			System.out.println("End Test");
+			System.exit(0);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+	}
+
+}
